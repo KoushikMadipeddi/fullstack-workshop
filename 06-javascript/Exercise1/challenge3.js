8f48cbb2b763177130891bed6f2406b70d97e7be
koushik <koushik@gmail.com>
2025-12-29 14:25:51 +0530

    adding new file of js

---
+++ 06-javascript/Exercise1/challenge3.js
@@ -0,0 +1,24 @@
+// Create a 3-question quiz
+// Keep track of the score
+// Display final score at the end
+
+// Example questions:
+// 1. What is 5 + 3?
+// 2. What is the capital of France?
+// 3. What color is the sky?
+
+let num = Number(prompt(`What is 5 + 3?`));
+let capital = prompt(`What is the capital of France?`);
+let colour = prompt(` What color is the sky?`);
+
+var sum =0;
+if(num===8){
+    sum++;
+}
+if(capital==='france'){
+    sum++;
+}
+if(colour==='blue'){
+    sum++;
+}
+console.log(sum);
