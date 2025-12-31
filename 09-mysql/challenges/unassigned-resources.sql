d4d8a4fe50b790d09046308078c0f39f6aa57246
KoushikMadipeddi <152767409+KoushikMadipeddi@users.noreply.github.com>
2025-12-31 10:41:17 +0530

    Update unassigned-resources.sql

--- 09-mysql/challenges/unassigned-resources.sql
+++ 09-mysql/challenges/unassigned-resources.sql
@@ -1,7 +1,7 @@
 SELECT
-    e.id,
-    e.name,
-    e.department
+    e.id as id,
+    e.name as name,
+    e.department as department
 FROM employees e
 LEFT JOIN assignments a
     ON e.id = a.employee_id
