9edd29da8862eaf9223675401f23d0d920c8d535
KoushikMadipeddi <152767409+KoushikMadipeddi@users.noreply.github.com>
2025-12-31 10:28:16 +0530

    Update department-states.sql

--- 09-mysql/challenges/department-states.sql
+++ 09-mysql/challenges/department-states.sql
@@ -1,8 +1,18 @@
+-- SELECT
+-- department,
+-- COUNT(*) AS employee_count,
+-- round(avg(salary),2) AS avg_salary,
+-- max(salary) AS max_salary
+-- FROM employees
+-- GROUP BY department
+-- HAVING COUNT(*)>2;
+
 SELECT
-department,
-COUNT(*) AS employee_count,
-round(avg(salary),2) AS avg_salary,
-max(salary) AS max_salary
-FROM employees
-GROUP BY department
-HAVING COUNT(*)>2;
+    d.dept_name AS department,
+    COUNT(e.emp_id) AS employee_count,
+    ROUND(AVG(e.salary), 2) AS avg_salary,
+    MAX(e.salary) AS max_salary
+FROM employees e
+JOIN departments d ON e.dept_id = d.dept_id
+GROUP BY d.dept_name
+HAVING COUNT(e.emp_id) > 2;
